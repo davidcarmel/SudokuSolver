4068807f67645e7a8b2a2901e428baf7b3fe2e75
David Carmel <dacarmel@amazon.com>
2021-09-20 11:32:09 +0300

    batch file

--- sudoku.bat
+++ sudoku.bat
@@ -1,15 +1,15 @@
-# bash!
+#!/bin/bash
+#
+# Usage: ./sudoku.bat ./data/95puzzles/p1.txt 3 "runh"
 # parameters: 
 #	$1 - file name 
 #	$2 - BlockSize  (3,4, etc.)
-#
-#	$3 strategies "ruhn"
+#	$3 strategies "[ruhn]"
 #		"" no strategies - only backtracking (run might take a long time)
 #		r - Candidate Reduction
 #		u - Uniqueness
 #		h - Hidden Pairs
 #		n - NamkedPairs
-# Usage: ./sudoku.bat ./data/95puzzles/p1.txt 3 "runh"
 #
 #
 java -cp sudokuSolver.jar com.io.sudoku.SudokuSolver -f $1 -b $2 -s $3
